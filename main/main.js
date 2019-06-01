affd9e0059dc9d230bc9d8050c3b88922bbc6896
chuzhuqin <2627824203@qq.com>
2019-06-01 23:12:21 +0800

    finished main.js in test 3

--- main/main.js
+++ main/main.js
@@ -1,17 +1,17 @@
 module.exports = function main(inputs) {
-    var fee1 = dfee(inputs['distance']);
-    var fee2 = wfee(inputs['parkTime']);
-
-    return Math.round(fee1+fee2);
+    var fee1 = getfee(inputs['distance']);
+    var fee2 = 0.25*(inputs['parkTime']);
+    var fee = Math.round(fee1+fee2)
+    return fee;
 };
 
-function dfee (distance) {
+function getfee (distance) {
 	var fee = 6;
-	fee += distance > 2 ? (distance-2)*0.8 : 0;
-	fee += distance > 8 ? (distance-8)*0.4 : 0;
+	if (distance > 2) {
+		fee = fee + 0.8*(distance-2);
+	}
+	if (distance > 8) {
+		fee = fee + 0.4*(distance-8);
+	}
 	return fee;
-};
-
-function wfee (time) {
-	return 0.25*time;
 };
